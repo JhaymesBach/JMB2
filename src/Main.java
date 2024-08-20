71a997488a2faf3b88be593af5d1600b7ed672c0
iurigiroldi <iurigiroldi@gmail.com>
2024-08-19 22:14:39 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,17 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+int idadePessoa1= 31;
+double altura= 1.81;
+string nome= "Calleri";
+boolean estudante= true;
+
+int idadePessoa2= 32;
+int somaIdades= idadePessoa1 + idadePessoa2;
+double alturaDobro= alturaPessoa * 2;
+System.out.println("idade: " + idadePessoa1);
+System.out.println("altura: " + altura);
+System.out.println("nome: " + nome);
+System.out.println("estudante: " + estudante);
+System.out.println("soma das idades: " + somaIdades);
+System.out.println("altura dobrada: " + alturaDobro);
